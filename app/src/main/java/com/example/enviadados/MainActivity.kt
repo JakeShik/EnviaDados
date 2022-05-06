25a4737215fb1f8b383a17c4c84055ce598c58bd
1700948 <jaha5825@gmail.com>
2022-05-06 11:52:20 +0100

    Added layout, fixed manifest, showing just name for now.

--- app/src/main/java/com/example/enviadados/MainActivity.kt
+++ app/src/main/java/com/example/enviadados/MainActivity.kt
@@ -1,17 +1,31 @@
 package com.example.enviadados
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
+import android.widget.CalendarView
 import android.widget.EditText
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private var dataNascimento: Long = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val enviarButton = findViewById<Button>(R.id.buttonEnviar)
-        enviarButton.setOnClickListener{enviaDados()}
+        enviarButton.setOnClickListener{    enviaDados()    }
+
+        val calendarViewDataDeNascimento = findViewById<CalendarView>(R.id.calendarViewDataNascimento)
+        dataNascimento = calendarViewDataDeNascimento.date
+
+        calendarViewDataDeNascimento.setOnDateChangeListener{calendarView, ano,mes,dia ->
+            val calendario = Calendar.getInstance()
+            calendario.set(ano, mes, dia)
+            dataNascimento = calendario.timeInMillis
+        }
     }
 
 
@@ -27,6 +41,8 @@ class MainActivity : AppCompatActivity() {
         val telefone = editTextTelefone.text.toString()
 
 
+
+
         if (nome.isBlank()) {
             editTextNome.error = getString(R.string.nome_obrigatorio)
             editTextNome.requestFocus()
@@ -34,15 +50,33 @@ class MainActivity : AppCompatActivity() {
         }
 
         if (email.isBlank()) {
-            editTextEmail.error = getString(R.string.nome_obrigatorio)
+            editTextEmail.error = getString(R.string.email_obrigatorio)
             editTextEmail.requestFocus()
             return
         }
 
-        if (telefone.isBlank()) {
-            editTextTelefone.error = getString(R.string.nome_obrigatorio)
+        val telefoneValido = Patterns.PHONE.matcher(telefone).matches()
+        if (!telefoneValido) {
+            editTextTelefone.error = getString(R.string.telefone_invalido)
             editTextTelefone.requestFocus()
             return
         }
+        val intent = Intent(this, mostrarDados::class.java)
+
+
+        intent.putExtra(EXTRA_NOME, nome)
+        intent.putExtra(EXTRA_EMAIL, email)
+        intent.putExtra(EXTRA_TELEFONE, telefone)
+        intent.putExtra(EXTRA_DATA_NASCIMENTO, dataNascimento)
+
+        startActivity(intent)
+
+
+    }
+    companion object{
+        const val EXTRA_NOME = "NOME"
+        const val EXTRA_EMAIL = "EMAIL"
+        const val EXTRA_TELEFONE = "TELEFONE"
+        const val EXTRA_DATA_NASCIMENTO = "DATA"
     }
 }
--- app/src/main/java/com/example/enviadados/mostrarDados.kt
+++ app/src/main/java/com/example/enviadados/mostrarDados.kt
@@ -1,4 +1,19 @@
 package com.example.enviadados
 
-class mostrarDados {
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class mostrarDados : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mostra_dados)
+
+
+        val nome = intent.getStringExtra(MainActivity.EXTRA_NOME)
+        val textViewNome = findViewById<TextView>(R.id.textViewNome)
+        textViewNome.text = nome
+    }
+
+
 }
